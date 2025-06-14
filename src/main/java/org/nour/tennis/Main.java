8247616fa037e08c2c91ad25718f74e5c849ab2a
Nour Krichene <krichenenour2@gmail.com>
2025-06-14 20:26:22 +0200

    Edit the Main class

--- src/main/java/org/nour/tennis/Main.java
+++ src/main/java/org/nour/tennis/Main.java
@@ -1,13 +1,16 @@
 package org.nour.tennis;
 
+import java.util.Scanner;
+
 import static org.nour.tennis.GameScoreEvaluator.computeGameScore;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        System.out.println(computeGameScore("ABABAA"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter players' points win history (e.g., ABABAA): ");
+        String input = scanner.nextLine();
+        System.out.println(computeGameScore(input));
     }
-
 
 }
