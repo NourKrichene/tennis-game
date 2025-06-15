6032f2a67b4c604feddda3456683fa3b3af2fa19
Nour Krichene <krichenenour2@gmail.com>
2025-06-15 15:12:15 +0200

    Extract the writing lines into a separate method

--- src/main/java/org/nour/tennis/GameScoreEvaluator.java
+++ src/main/java/org/nour/tennis/GameScoreEvaluator.java
@@ -7,6 +7,7 @@ public class GameScoreEvaluator {
 
     private static final int DEUCE_START_POINT = 6;
     private static final String INVALID_INPUT_MESSAGE = "Invalid input : must be not empty and contain only A or B";
+    private static final String DEUCE = "DD";
 
     public static String computeGameScore(String playersPointsWinHistory) {
         if (!isValid(playersPointsWinHistory)) {
@@ -15,62 +16,61 @@ public class GameScoreEvaluator {
 
         PreDeuceGameScore preDeuceGameScore = computePreDeuceGameScore(playersPointsWinHistory);
 
-        if (preDeuceGameScore.isDeuceReached) {
-            String gameDeuceScore = computeDeuceGameScore(playersPointsWinHistory);
-            return preDeuceGameScore.scoreLines().concat(gameDeuceScore);
-        }
+        String score = preDeuceGameScore.isDeuceReached ?
+                preDeuceGameScore.score().concat(computeDeuceGameScore(playersPointsWinHistory)) :
+                preDeuceGameScore.score();
 
-        return preDeuceGameScore.scoreLines();
+        return formatScoreToLines(score);
     }
 
     private static PreDeuceGameScore computePreDeuceGameScore(String playersPointsWinHistory) {
         int[] playersPoints = new int[2];
-        var scoreLines = new StringBuilder();
+        var score = new StringBuilder();
 
         for (int i = 0; i < playersPointsWinHistory.length(); i++) {
             char currentPlayer = playersPointsWinHistory.charAt(i);
             int currentPlayerPoints = ++playersPoints[currentPlayer - 'A'];
 
             if (isWinPointReached(currentPlayerPoints)) {
-                scoreLines.append(writeWinnerLine(currentPlayer));
-                return new PreDeuceGameScore(scoreLines.toString(), false);
+                score.append(currentPlayer).append(WIN);
+                return new PreDeuceGameScore(score.toString(), false);
             }
 
             if (isDeuceReached(playersPoints)) {
-                return new PreDeuceGameScore(scoreLines.toString(), true);
+                return new PreDeuceGameScore(score.toString(), true);
             }
 
-            scoreLines.append(writeScoreLine(playersPoints));
+            score.append(playersPoints[0]).append(playersPoints[1]);
         }
 
-        return new PreDeuceGameScore(scoreLines.toString(), false);
+        return new PreDeuceGameScore(score.toString(), false);
     }
 
     private static String computeDeuceGameScore(String playersPointsWinHistory) {
-        var scoreLines = new StringBuilder(writeDeuceLine());
+        var score = new StringBuilder(DEUCE);
         boolean isPreviousPointDeuce = true;
 
         for (int i = DEUCE_START_POINT; i < playersPointsWinHistory.length(); i++) {
             char currentPlayer = playersPointsWinHistory.charAt(i);
 
             if (isPreviousPointDeuce) {
-                scoreLines.append(writeAdvantageLine(currentPlayer));
+                score.append(currentPlayer).append(ADVANTAGE);
                 isPreviousPointDeuce = false;
                 continue;
             }
 
             if (twoConsecutivePointsWon(playersPointsWinHistory, i)) {
-                scoreLines.append(writeWinnerLine(currentPlayer));
-                return scoreLines.toString();
+                score.append(currentPlayer).append(WIN);
+                return score.toString();
             }
 
-            scoreLines.append(writeDeuceLine());
+            score.append(DEUCE);
             isPreviousPointDeuce = true;
         }
 
-        return scoreLines.toString();
+        return score.toString();
     }
 
-    private record PreDeuceGameScore(String scoreLines, boolean isDeuceReached) {
+    private record PreDeuceGameScore(String score, boolean isDeuceReached) {
     }
 }
--- src/main/java/org/nour/tennis/util/ScoreWriterUtil.java
+++ src/main/java/org/nour/tennis/util/ScoreWriterUtil.java
@@ -3,22 +3,43 @@ package org.nour.tennis.util;
 
 public class ScoreWriterUtil {
 
+    public static final char WIN = 'W';
+    public static final char ADVANTAGE = 'a';
+    private static final char DEUCE = 'D';
     private static final String NEW_LINE = System.lineSeparator();
     private static final int[] POINTS = new int[]{0, 15, 30, 40};
 
-    public static String writeScoreLine(int[] score) {
-        return String.format("Player A : %d / Player B : %d%s", POINTS[score[0]], POINTS[score[1]], NEW_LINE);
+    public static String formatScoreToLines(String score) {
+        var scoreLines = new StringBuilder();
+
+        for (int i = 0; i < score.length(); i += 2) {
+            char firstSymbol = score.charAt(i);
+            char secondSymbol = score.charAt(i + 1);
+
+            scoreLines.append(switch (secondSymbol) {
+                case ADVANTAGE -> writeAdvantageLine(firstSymbol);
+                case DEUCE -> writeDeuceLine();
+                case WIN -> writeWinnerLine(firstSymbol);
+                default -> writeScoreLine(POINTS[firstSymbol - '0'], POINTS[secondSymbol - '0']);
+            });
+
+        }
+        return scoreLines.toString();
     }
 
-    public static String writeWinnerLine(char currentPlayer) {
+    private static String writeScoreLine(int pointsPlayerA, int pointsPlayerB) {
+        return String.format("Player A : %d / Player B : %d%s", pointsPlayerA, pointsPlayerB, NEW_LINE);
+    }
+
+    private static String writeWinnerLine(char currentPlayer) {
         return String.format("Player %c wins the game", currentPlayer);
     }
 
-    public static String writeAdvantageLine(char currentPlayer) {
+    private static String writeAdvantageLine(char currentPlayer) {
         return String.format("Player %c Advantage%s", currentPlayer, NEW_LINE);
     }
 
-    public static String writeDeuceLine() {
+    private static String writeDeuceLine() {
         return "Deuce" + NEW_LINE;
     }
 }
